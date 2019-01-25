77de390d57f2cc039f21e098401c219a3fdbeeb4
Vince Knight <vincent.knight@gmail.com>
2019-01-25 16:15:51 +0000

    Ignore checkpoint files in tests.

--- tests/test_gen_notebook_files_in_dir.py
+++ tests/test_gen_notebook_files_in_dir.py
@@ -8,4 +8,7 @@ def test_gen_notebook_files_in_dir():
 
     nbs = blackbook.gen_notebook_files_in_dir(path)
     expected_nbs_names = sorted(["spaces.ipynb", "formatted_spaces.ipynb"])
-    assert sorted([nb.parts[-1] for nb in nbs]) == expected_nbs_names
+    assert (
+        sorted([nb.parts[-1] for nb in nbs if "checkpoint" not in str(nb)])
+        == expected_nbs_names
+    )
