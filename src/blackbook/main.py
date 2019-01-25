eee6670485921a86326bf484fb5ac39f9a9b41be
Vince Knight <vincent.knight@gmail.com>
2019-01-25 16:39:47 +0000

    Write function to reformat notebook.

--- src/blackbook/main.py
+++ src/blackbook/main.py
@@ -4,15 +4,25 @@ from typing import Iterator
 
 import re
 import pathlib
+import json
 
 import black
 
 
-JUPYTER_REGEX = re.compile(r"\.ipynbi?$")
-
-
-def gen_notebook_files_in_dir(
-        path: pathlib.Path,
-) -> Iterator[pathlib.Path]:
+def gen_notebook_files_in_dir(path: pathlib.Path) -> Iterator[pathlib.Path]:
 
     return path.glob("**/*.ipynb")
+
+def format_notebook_content(path: pathlib.Path) -> dict:
+    content = path.read_text()
+    nb = json.loads(content)
+
+    for cell in nb["cells"]:
+        try:
+            string = "".join(cell["source"])
+            formatted_string = black.format_str(string, line_length=black.DEFAULT_LINE_LENGTH)
+            cell["source"] = [s + "\n" for s in formatted_string.split("\n")][:-1]
+        except black.InvalidInput:
+            pass
+
+    return nb
--- tests/test_format_notebook_content.py
+++ tests/test_format_notebook_content.py
@@ -5,11 +5,15 @@ import blackbook
 
 
 def test_format_notebook_content():
-    data_path = pathlib.Path(f'{__file__}/data/')
-    source_notebook_path = data_path / 'spaces.ipynb'
+    data_path = pathlib.Path(f"{__file__}").parent / "data"
+    source_notebook_path = data_path / "spaces.ipynb"
     output_json = blackbook.format_notebook_content(source_notebook_path)
 
-    formatted_notebook_path = data_path / 'formatted_spaces.ipynb'
-    expected_json = json.load(formatted_notebook_path)
+    formatted_notebook_path = data_path / "formatted_spaces.ipynb"
+    expected_content = formatted_notebook_path.read_text()
+    expected_json = json.loads(expected_content)
 
-    assert output_json == expected_json
+    print(expected_json["cells"])
+    print(output_json["cells"])
+    assert all([cell["source"] == expected_cell["source"] for cell, expected_cell
+            in zip(output_json["cells"], expected_json["cells"])])
