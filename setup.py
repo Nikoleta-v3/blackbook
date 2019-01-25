bf2557df22acea694003342c822f9ef1163526f3
Vince Knight <vincent.knight@gmail.com>
2019-01-25 17:29:59 +0000

    Remove git diff marker.

--- setup.py
+++ setup.py
@@ -5,12 +5,7 @@ import os
 
 requirements = ["black>=18.9b0"]
 
-# Read in the version number
-<<<<<<< 4419fc7fdbd89008a0eb9b57b584a00b873b86df
-exec(open("axelrod/version.py", "r").read())
-=======
 exec(open("src/blackbook/version.py", "r").read())
->>>>>>> Add a version number.
 
 setup(
     name="blackbook",
