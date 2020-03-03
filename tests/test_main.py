1415a23eb04fa50587e4671c617a1c33fbb729f8
Henry Wilde <henrydavidwilde@gmail.com>
2020-03-03 16:54:18 +0000

    Remove `test_main.py` to show Action works.

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-
-from blackbook.__main__ import main
-
-
-def test_main_runs_without_failure():
-    data_path = pathlib.Path(f"{__file__}").parent / "data" / "formatted"
-
-    assert main(data_path) is None
