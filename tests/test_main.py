70c24565810013b1c11bc72b46c71623b6f1c381
Matthew Feickert <matthew.feickert@cern.ch>
2020-04-15 23:21:01 -0500

    Add test for invalid paths

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 import pathlib
 
+import pytest
+
 from blackbook.__main__ import main
 
 
@@ -7,3 +9,9 @@ def test_main_runs_without_failure():
     data_path = pathlib.Path(f"{__file__}").parent / "data" / "formatted"
 
     assert main(data_path) is None
+
+
+def test_invalid_path():
+    with pytest.raises(OSError):
+        data_path = pathlib.Path(f"{__file__}").parent / "invalid"
+        main(data_path)
