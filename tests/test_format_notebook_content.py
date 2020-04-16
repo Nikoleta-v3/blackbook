bc484a162918fc64c3da2f039a930922bf04d29e
Matthew Feickert <matthew.feickert@cern.ch>
2020-04-16 00:22:09 -0500

    Add test for formatting of single notebook
    Copy the unformatted spaces notebook and first check that the copy and
    the source are the same. Then run blackbook's main with the path being
    the full path to spaces.ipynb. Then check that the contents of the
    formatted spaces.ipynb differs from the copied notebook, which has been
    untouched by blackbook. Also delete the copied notebook to make test
    atomic.

--- tests/test_format_notebook_content.py
+++ tests/test_format_notebook_content.py
@@ -1,5 +1,6 @@
 import json
 import pathlib
+import shutil
 
 import blackbook
 
@@ -16,6 +17,43 @@ def test_format_notebook_content():
         [
             cell["source"] == expected_cell["source"]
             for cell, expected_cell in zip(output_json["cells"], expected_json["cells"])
+        ]
+    )
+
+
+def test_format_single_notebook():
+    """
+    Test formatting a single notebook in an directory of unformatted notebooks
+    formats only that notebook
+    """
+    data_path = pathlib.Path(f"{__file__}").parent / "data"
+    source_notebook_path = data_path / "unformatted" / "spaces.ipynb"
+    copy_notebook_path = data_path / "unformatted" / "spaces_copy.ipynb"
+    shutil.copy(str(source_notebook_path), str(copy_notebook_path))
+
+    source_json = json.loads(source_notebook_path.read_text())
+    copy_json = json.loads(copy_notebook_path.read_text())
+
+    assert all(
+        [
+            copy_cell["source"] == source_cell["source"]
+            for copy_cell, source_cell in zip(copy_json["cells"], source_json["cells"])
+        ]
+    )
+
+    # Have to run blackbook main to show spaces_copy.ipynb is unaffected
+    blackbook.__main__.main(source_notebook_path)
+    output_json = json.loads(source_notebook_path.read_text())
+    check_copy_json = json.loads(copy_notebook_path.read_text())
+    shutil.copy(str(copy_notebook_path), str(source_notebook_path))
+    pathlib.Path.unlink(copy_notebook_path)
+
+    assert not all(
+        [
+            formatted_cell["source"] == unformatted_cell["source"]
+            for formatted_cell, unformatted_cell in zip(
+                output_json["cells"], check_copy_json["cells"]
+            )
         ]
     )
 
