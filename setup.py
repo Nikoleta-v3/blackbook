2d8001073c9eb65a36772d6731beb6bce010e2b1
Matthew Feickert <matthew.feickert@cern.ch>
2020-04-17 12:36:14 -0500

    fix: Improve PyPI page render (#38)
    * Fix rendering of README on PyPI
    
    * Add Python supported versions to PyPI metadata

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 import doctest
 import os
 import unittest
+from pathlib import Path
 
 from setuptools import find_packages, setup
 
 requirements = ["black>=18.9b0", "loguru>=0.2.5"]
 
-with open("README.md", "r") as readme:
-    README = readme.read()
+this_directory = Path(__file__).parent.resolve()
+with open(Path(this_directory).joinpath("README.md"), encoding="utf-8") as readme_md:
+    README = readme_md.read()
 
 exec(open("src/blackbook/version.py", "r").read())
 
@@ -23,5 +25,12 @@ setup(
     license="The MIT License (MIT)",
     description="`Black` for Jupyter notebooks.",
     long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
     entry_points={"console_scripts": "blackbook=blackbook.__main__:main"},
 )
